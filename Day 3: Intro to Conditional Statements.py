6eae87cfcfeacfa78f004a242e5754c2d255663f
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-09-21 10:33:25 -0400

    Create Day 3: Intro to Conditional Statements.py

---
+++ Day 3: Intro to Conditional Statements.py
@@ -0,0 +1,25 @@
+#Problem Link: https://www.hackerrank.com/challenges/30-conditional-statements/problem?isFullScreen=true
+
+#Ans:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
