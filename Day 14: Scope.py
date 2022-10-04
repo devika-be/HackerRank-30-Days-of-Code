930db923cb819bb11e729b0ee8a3fe12bfae57e9
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-10-04 19:25:49 +0530

    Create Day 14: Scope.py

---
+++ Day 14: Scope.py
@@ -0,0 +1,32 @@
+#Problem Link: https://www.hackerrank.com/challenges/30-scope/problem?isFullScreen=true
+
+#Ans:
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        
+        
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+    # Add your code here
+    def computeDifference(self):
+        l = len(a)
+        for i in range(0, l):
+            for j in range(i + 1, l):
+                difference = abs(a[i] - a[j])
+                self.maximumDifference = max(difference, self.maximumDifference)
+	# Add your code here
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
